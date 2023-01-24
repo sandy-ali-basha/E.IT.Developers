550a9c512bc5699f2618501607d2546f82918159
sandy <sandy.ali.basha@gmail.com>
2023-01-24 22:40:26 +0200

    adding flters to our work section cards

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $('.ViewMore').click(function () {
+        $('.portfolioCard').removeClass("d-none");
+        $(this).addClass('d-none')
+    });
     $('.nav-item .nav-link').click(function () {
         $('.nav-link').removeClass("active");
         $(this).addClass("active");
@@ -26,3 +30,38 @@ $(document).ready(function () {
     });
 })
 
+
+
+
+$('.filters a').click(function () {
+    $('.filters a').removeClass('PSectionActive');
+    $(this).addClass('PSectionActive');
+
+    var data = $(this).attr('data-filter');
+    $grid.isotope({
+        filter: data
+    })
+});
+
+var $grid = $(".grid").isotope({
+    itemSelector: ".all",
+    percentPosition: true,
+    masonry: {
+        columnWidth: ".all"
+    }
+})
+$(document).on("click", ".naccs .menu div", function () {
+    var numberIndex = $(this).index();
+    if (!$(this).is("active")) {
+        $(".naccs .menu div").removeClass("active");
+        $(".naccs ul li").removeClass("active");
+
+        $(this).addClass("active");
+        $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+        var listItemHeight = $(".naccs ul")
+            .find("li:eq(" + numberIndex + ")")
+            .innerHeight();
+        $(".naccs ul").height(listItemHeight + "px");
+    }
+});
